954ba016e54bf63d394324d38b46406ee4637d83
Zimtir <zimtir@mail.ru>
2019-10-06 00:42:08 +0300

    add client-input Svelte

---
+++ client/client-input/src/App.svelte
@@ -0,0 +1,27 @@
+﻿<script>
+  import Api from '../../../common/tools/api.tool.ts'
+
+  let siteUrl = ''
+  let selector = ''
+  const parseSite = async () => {
+    await Api.generate(siteUrl, selector)
+  }
+</script>
+
+<style>
+  .btn {
+    font-size: 18px;
+    height: 36px;
+  }
+
+  button {
+    color: sienna;
+    width: 40px;
+    background: white;
+    border-radius: 50%;
+  }
+</style>
+
+<input class="btn" bind:value={siteUrl} placeholder="Site url" />
+<input class="btn" bind:value={selector} placeholder="Selector" />
+<button on:click={parseSite} class="btn">></button>
