c43ac0993e97e866c994939c64d55f51cf9f00d7
Zimtir <zimtir@mail.ru>
2019-10-06 18:33:15 +0300

    update client

---
+++ client/src/App.svelte
@@ -0,0 +1,22 @@
+﻿<script>
+  import Modal from 'svelte-simple-modal'
+
+  import Form from './components/Form.svelte'
+  import ItemList from './components/ItemList.svelte'
+
+  import Api from '../../common/tools/api.tool'
+
+  let items = []
+
+  const parseSite = async event => {
+    console.log(event)
+    items = await Api.generate(event.detail.site, event.detail.selector)
+    console.log(items)
+  }
+</script>
+
+<Modal>
+  <Form on:done={parseSite} />
+
+  <ItemList {items} />
+</Modal>
--- client/src/index.ts
+++ client/src/index.ts
@@ -3,7 +3,7 @@ import App from './App.svelte'
 
 var app = new App({
   //@ts-ignore
-  target: document.querySelector('#client-input')
+  target: document.querySelector('#client')
 })
 
 export default app
